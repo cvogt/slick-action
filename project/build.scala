991be7d2c51453e2cb520c1afb31a07dd4b5eb8b
Jan Christopher Vogt <oss.nsp@cvogt.org>
2014-09-12 14:31:53 -0400

    Remove unnecessary dependencies (especially noop logger)

--- project/build.scala
+++ project/build.scala
@@ -12,17 +12,16 @@ object MyBuild extends Build{
       description := "Automatic connection management preview",
       libraryDependencies ++= Seq(
         "com.typesafe.slick" %% "slick" % "2.1.0"
-        ,"com.typesafe.slick" %% "slick-codegen" % "2.1.0"
         ,"org.scalatest" %% "scalatest" % "2.1.6" % "test"
-        ,"org.slf4j" % "slf4j-nop" % "1.6.4"
-        ,"com.h2database" % "h2" % "1.3.170"
+        ,"org.slf4j" % "slf4j-nop" % "1.6.4" % "test"
+        ,"com.h2database" % "h2" % "1.3.170" % "test"
       ),
       libraryDependencies <+= scalaVersion(
         "org.scala-lang" % "scala-reflect" % _
       ),
       scalacOptions ++= Seq("-feature", "-deprecation", "-unchecked"),
       scalaVersion := "2.10.4",
-      version := "0.1",
+      version := "0.1.1",
       organizationName := "Christopher Vogt",
       organization := "org.cvogt",
       scalacOptions in (Compile, doc) <++= (version,sourceDirectory in Compile,name).map((v,src,n) => Seq(
